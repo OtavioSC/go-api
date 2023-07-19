645049eb31caebf333d4ba8e2190b5a39a00d555
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-19 14:39:24 -0300

    feat: extract handlers for their own files

--- handler/handler.go
+++ handler/handler.go
@@ -1,37 +1,4 @@
 package handler
 
-import (
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
 
-func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
-}
-
-func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CREATE opening",
-	})
-}
-
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE opening",
-	})
-}
-
-func PutOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "PUT opening",
-	})
-}
-
-func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "LIST openings",
-	})
-}
