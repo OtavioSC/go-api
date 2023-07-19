b34831b93371534ab02ab25543cca228036ef147
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-19 15:56:55 -0300

    feat: create SQLite database handler

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	logger = *config.GetLogger("main")
 	err := config.Init()
 	if err != nil {
-		logger.Errorf("Config initialization error: %v", err)
+		logger.Errorf("config initialization error: %v", err)
 		return
 	}
 
