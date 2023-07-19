3a6a1ae3cb7eef22f67dbd488bf93977ddf58fef
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-19 17:42:29 -0300

    feat: add get by id api method handler

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,11 +3,24 @@ package handler
 import (
 	"net/http"
 
+	"github.com/OtavioSC/go-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", 
+		"queryParamater").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error;err!=nil{
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
