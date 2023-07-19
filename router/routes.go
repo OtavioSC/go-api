39503f3b20edd8f26461e4770f3c37ac55874394
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-19 15:35:56 -0300

    update: change put route name to update route

--- router/routes.go
+++ router/routes.go
@@ -11,7 +11,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-		v1.PUT("/opening", handler.PutOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
