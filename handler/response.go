21941b7d9b01105746f17ab2e75fdaf38d418ba6
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-19 16:43:54 -0300

    feat: add success response handler

--- handler/response.go
+++ handler/response.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func sendError(ctx *gin.Context, code int, msg string) {
 	ctx.Header("Content-type", "application/json")
@@ -8,5 +13,14 @@ func sendError(ctx *gin.Context, code int, msg string) {
 	ctx.JSON(code, gin.H{
 		"message": msg,
 		"errorCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-type", "application/json")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation from handler %s successfull", op),
+		"data": data,
 	})
 }
