c9e22d274727a031396eb5c277a10c32110dc73f
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-19 16:37:47 -0300

    feat: create opening creation handler specifies

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,30 @@ package handler
 import (
 	"net/http"
 
+	"github.com/OtavioSC/go-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CREATE opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	request.Validate()
+
+	if err := request.Validate(); err!=nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+
+	}
+
+	if err := db.Create(&request).Error; err!=nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
 }
--- handler/handler.go
+++ handler/handler.go
@@ -1,4 +1,16 @@
 package handler
 
+import (
+	"github.com/OtavioSC/go-api/config"
+	"gorm.io/gorm"
+)
 
+var (
+	logger *config.Logger
+	db *gorm.DB
+)
 
+func InitiliazeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
